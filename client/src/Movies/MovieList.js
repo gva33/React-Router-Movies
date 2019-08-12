006256a2513825d748fa46e3359738ba0c95021d
gva33 <galcantara33@gmail.com>
2019-08-11 19:47:27 -0700

    mvp i think

--- client/src/Movies/MovieList.js
+++ client/src/Movies/MovieList.js
@@ -1,35 +1,38 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
+import {NavLink} from 'react-router-dom'
 
 const MovieList = props => {
   const [movies, setMovies] = useState([])
-  useEffect(() => {
-    const getMovies = () => {
-      axios
-        .get('http://localhost:5000/api/movies')
-        .then(response => {
-          setMovies(response.data);
-        })
-        .catch(error => {
-          console.error('Server Error', error);
-        });
-    }
-    
-    getMovies();
-  }, []);
-  
-  return (
-    <div className="movie-list">
-      {movies.map(movie => (
-        <MovieDetails key={movie.id} movie={movie} />
-      ))}
-    </div>
-  );
+ useEffect(() => {
+   const getMovies = () => {
+     axios
+       .get('http://localhost:5000/api/movies')
+       .then(response => {
+         setMovies(response.data);
+       })
+       .catch(error => {
+         console.error('Server Error', error);
+       });
+   }
+
+   getMovies();
+ }, []);
+
+ return (
+   <div className="movie-list">
+     {movies.map(movie => (
+       <MovieDetails key={movie.id} movie={movie} />
+
+     ))}
+   </div>
+ );
 }
 
 function MovieDetails({ movie }) {
-  const { title, director, metascore, stars } = movie;
+  const { title, director, metascore, stars, id } = movie;
   return (
+    <NavLink to={`/movies/${id}`}>
     <div className="movie-card">
       <h2>{title}</h2>
       <div className="movie-director">
@@ -46,6 +49,7 @@ function MovieDetails({ movie }) {
         </div>
       ))}
     </div>
+    </NavLink>
   );
 }
 
